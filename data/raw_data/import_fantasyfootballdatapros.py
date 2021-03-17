54be0157a604614f9d2e88642b78fa34f98f9794
aidan-oconnor <o'connor_aidan@bah.com>
2021-03-17 00:15:58 -0400

    pushing python script that requests data from fantasy football data pros

---
+++ data/raw_data/import_fantasyfootballdatapros.py
@@ -0,0 +1,23 @@
+import requests
+
+import time
+
+def download_url(url, save_path):
+ '''
+ Purpose: Download a zip file of fantasy football data.
+ First try using the requests library to access the site.
+ If that fails, sleep for 5 seconds then re-try.
+ Finally, iterate through the file and write it to the intended directory'''
+    try:
+        r = requests.get(url, stream=True)
+    except:
+        time.sleep(5)
+    finally:
+        with open(save_path, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=100):
+                fd.write(chunk)
+
+download_url(
+    'https://www.fantasyfootballdatapros.com/static/data_v2.zip',
+             '/Users/aidanoconnor/GU/Cloudy-with-a-Chance-of-Football/data/raw_data/fantasy_football_data_pros.zip'
+)
