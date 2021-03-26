616e2f027ccf3411c602f21394211781c7351b4b
aidan-oconnor <o'connor_aidan@bah.com>
2021-03-25 21:17:57 -0400

    restructured and cleaned up the file structure

---
+++ tests/__init__.py
@@ -0,0 +1,25 @@
+import unittest
+
+'''
+run these tests by running the following commands in terminal:
+
+nosetests -v --with-coverage --cover-package=emailgraph \
+--cover-inclusive --cover-erase tests
+'''
+
+class InitializationTests(unittest.TestCase):
+
+    def test_initialization(self):
+        """
+        Check the test suite runs by affirming 2+2=4
+        """
+        self.assertEqual(2+2, 4)
+
+    def test_import(self):
+        """
+        Ensure the test suite can import our module
+        """
+        try:
+            import emailgraph
+        except ImportError:
+            self.fail("Was not able to import")
