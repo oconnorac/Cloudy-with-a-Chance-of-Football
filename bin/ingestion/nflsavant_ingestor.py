7d8366899d208b87aed22ef7666893f63f48fd56
ytewahade <ytewahade@gmail.com>
2021-03-29 17:02:54 -0400

    added #shebang, and correct csv data to ingest.

--- bin/ingestion/nflsavant_ingestor.py
+++ bin/ingestion/nflsavant_ingestor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+#Using urllib, programmatically download/ingest CSV data.
 from urllib.request import urlretrieve as retrieve
 url = 'http://nflsavant.com/pbp_data.php?year=2019'
 retrieve (url,'nflsavant_201920-players.csv.')
