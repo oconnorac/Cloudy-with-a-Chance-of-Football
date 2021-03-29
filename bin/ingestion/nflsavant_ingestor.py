b47190529656c0d6ff2780f36d79a20f1d20aa69
ytewahade <ytewahade@gmail.com>
2021-03-29 16:07:23 -0400

    added shebang to script

--- bin/ingestion/nflsavant_ingestor.py
+++ bin/ingestion/nflsavant_ingestor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python 
+
 from urllib.request import urlretrieve as retrieve
 url = 'http://nflsavant.com/dump/players_2013-12-12.csv'
 retrieve (url,'nflsavant_2013-12-players.csv.')
