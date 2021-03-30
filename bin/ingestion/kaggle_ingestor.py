3ff440890802197a78586776a2c6b63da9ec7d14
griffin-taub <73346755+griffin-taub@users.noreply.github.com>
2021-03-30 08:44:16 -0400

    Update kaggle_ingestor.py

--- bin/ingestion/kaggle_ingestor.py
+++ bin/ingestion/kaggle_ingestor.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[26]:
-
 
 from urllib.request import urlretrieve as retrieve
 
@@ -11,16 +7,3 @@ positions = ['defense_stats_and_projections.csv', 'kicker_stats_and_projections.
 for i in positions:
     url = 'https://www.kaggle.com/mur418/espn-2019-stats-and-2020-nfl-fantasy-projections?select='
     retrieve (url,i)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
