5140c9142ff7dabc52cf9f76dcfaf8e758842cd8
griffin-taub <73346755+griffin-taub@users.noreply.github.com>
2021-03-30 08:35:44 -0400

    Add files via upload

---
+++ bin/ingestion/kaggle_ingestor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+from urllib.request import urlretrieve as retrieve
+
+positions = ['defense_stats_and_projections.csv', 'kicker_stats_and_projections.csv', 'qb_stats_and_projections.csv', 'rb_stats_and_projections.csv', 'te_stats_and_projections.csv', 'wr_stats_and_projections.csv']
+
+for i in positions:
+    url = 'https://www.kaggle.com/mur418/espn-2019-stats-and-2020-nfl-fantasy-projections?select='
+    retrieve (url,i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
