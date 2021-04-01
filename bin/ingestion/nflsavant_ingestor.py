9d60acbdce6431bb1f7e48e932b07bdb18f383b1
ytewahade@gmail.com <ytewahade@gmail.com>
2021-04-01 00:11:33 -0400

    cleaned up ingestion script to match pep style =th '#' will be ignored, and an empty message aborts the commit.

--- bin/ingestion/nflsavant_ingestor.py
+++ bin/ingestion/nflsavant_ingestor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 #Using urllib, programmatically download/ingest CSV data.
+#Be aware that script can only be used a limited number of times before IP address flagged.
 from urllib.request import urlretrieve as retrieve
 url = 'http://nflsavant.com/pbp_data.php?year=2019'
 retrieve(url,'nflsavant_201920-players.csv')
