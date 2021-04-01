f4712ffea502f742450f28258ea5ca65900b0cbb
ytewahade <ytewahade@gmail.com>
2021-04-01 00:00:09 -0400

    ing urllib, programmatically download/ingest CSV data. from urllib.request import urlretrieve as retrieve
    Please enter the commit message for your changes. Lines starting

--- bin/ingestion/nflsavant_ingestor.py
+++ bin/ingestion/nflsavant_ingestor.py
@@ -3,5 +3,5 @@
 #Using urllib, programmatically download/ingest CSV data.
 from urllib.request import urlretrieve as retrieve
 url = 'http://nflsavant.com/pbp_data.php?year=2019'
-retrieve (url,'nflsavant_201920-players.csv.')
+retrieve(url,'nflsavant_201920-players.csv')
 
